9a6dfc217ae9113d059e0a12a6c94b8fd5b9f775
Richard Garrison <anm8tr@users.noreply.github.com>
2023-06-27 14:25:10 -0700

    Add ISO8601FormatStyle top all dates

--- Sources/Jobs-App-SharedDTO/CreateNewJobRequestDTO.swift
+++ Sources/Jobs-App-SharedDTO/CreateNewJobRequestDTO.swift
@@ -7,13 +7,14 @@
 
 import Foundation
 
+@available(macOS 12.0, *)
 public struct CreateNewJobRequestDTO: Codable {
     
     public let companyName: String
     public let jobId: String
-    public let date: Date
+    public let date: Date.ISO8601FormatStyle
     
-    public init(companyName: String, jobId: String, date: Date) {
+    public init(companyName: String, jobId: String, date: Date.ISO8601FormatStyle) {
         self.companyName = companyName
         self.jobId = jobId
         self.date = date
--- Sources/Jobs-App-SharedDTO/CreateNewJobResponseDTO.swift
+++ Sources/Jobs-App-SharedDTO/CreateNewJobResponseDTO.swift
@@ -7,14 +7,15 @@
 
 import Foundation
 
+@available(macOS 12.0, *)
 public struct CreateNewJobResponseDTO: Codable {
     
     public let id: UUID
     public let companyName: String
     public let jobId: String
-    public let date: Date
+    public let date: Date.ISO8601FormatStyle
     
-    public init(id: UUID, companyName: String, jobId: String, date: Date) {
+    public init(id: UUID, companyName: String, jobId: String, date: Date.ISO8601FormatStyle) {
         self.id = id
         self.companyName = companyName
         self.jobId = jobId
