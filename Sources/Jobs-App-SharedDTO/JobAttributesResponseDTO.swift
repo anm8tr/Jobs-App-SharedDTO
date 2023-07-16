5c37664bbc99cc636d23474ca927e02b132987b2
Richard Garrison <anm8tr@users.noreply.github.com>
2023-07-16 13:33:01 -0700

    Update JobAttributesResponseDTO.swift

--- Sources/Jobs-App-SharedDTO/JobAttributesResponseDTO.swift
+++ Sources/Jobs-App-SharedDTO/JobAttributesResponseDTO.swift
@@ -15,20 +15,20 @@ public struct JobAttributesResponseDTO: Codable {
     public let address: String
     public let city: String
     public let state: String
-    public let zipcode: String
-    public let phone1: String
-    public let phone2: String
+    //public let zipcode: String
+    //public let phone1: String
+    //public let phone2: String
     public let email: String
     public let contacted: String
     public let appointmentSet: String
     public let appointmentDate: Date
     public let jobStatus: String
     public let invoiced: String
-    //public let invoiceAmount: Double
+    public let invoiceAmount: Double
     public let notes: String
     
     
-    public init(id: UUID, status: String, name: String, address: String, city: String, state: String, zipcode: String, phone1: String, phone2: String, email: String, contacted: String, appointmentSet: String, appointmentDate: Date, jobStatus: String, invoiced: String, /*invoiceAmount: Double,*/ notes: String) {
+    public init(id: UUID, status: String, name: String, address: String, city: String, state: String,/* zipcode: String, phone1: String, phone2: String,*/ email: String, contacted: String, appointmentSet: String, appointmentDate: Date, jobStatus: String, invoiced: String, invoiceAmount: Double, notes: String) {
         self.id = id
         self.status = status
         self.name = name
@@ -44,7 +44,7 @@ public struct JobAttributesResponseDTO: Codable {
         self.appointmentDate = appointmentDate
         self.jobStatus = jobStatus
         self.invoiced = invoiced
-        //self.invoiceAmount = invoiceAmount
+        self.invoiceAmount = invoiceAmount
         self.notes = notes
     }
     
