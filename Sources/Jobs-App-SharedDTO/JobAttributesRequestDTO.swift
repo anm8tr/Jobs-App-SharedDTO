5ceb974a977fb9d02a63bc299f31cbc9e7f3c97e
Richard Garrison <anm8tr@users.noreply.github.com>
2023-07-16 15:08:14 -0700

    Update JobAttributesRequestDTO.swift

--- Sources/Jobs-App-SharedDTO/JobAttributesRequestDTO.swift
+++ Sources/Jobs-App-SharedDTO/JobAttributesRequestDTO.swift
@@ -16,8 +16,8 @@ public struct JobAttributesRequestDTO: Codable {
     public let city: String
     public let state: String
     public let zipcode: String
-    public let phoneNumber: String
-    //public let phone2: String
+    public let phoneNumberOne: String
+    public let phoneNumberTwo: String
     public let email: String
     public let contacted: String
     public let appointmentSet: String
@@ -28,15 +28,15 @@ public struct JobAttributesRequestDTO: Codable {
     public let notes: String
     
     
-    public init(status: String, name: String, address: String, city: String, state: String, zipcode: String, phoneNumber: String, /*phone2: String,*/ email: String, contacted: String, appointmentSet: String, appointmentDate: Date, jobStatus: String, invoiced: String, invoiceAmount: Double, notes: String) {
+    public init(status: String, name: String, address: String, city: String, state: String, zipcode: String, phoneNumberOne: String, phoneNumberTwo: String, email: String, contacted: String, appointmentSet: String, appointmentDate: Date, jobStatus: String, invoiced: String, invoiceAmount: Double, notes: String) {
         self.status = status
         self.name = name
         self.address = address
         self.city = city
         self.state = state
         self.zipcode = zipcode
-        self.phoneNumber = phoneNumber
-        //self.phone2 = phone2
+        self.phoneNumberOne = phoneNumberOne
+        self.phoneNumberTwo = phoneNumberTwo
         self.email = email
         self.contacted = contacted
         self.appointmentSet = appointmentSet
