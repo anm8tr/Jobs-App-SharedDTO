50b2e16aade9040e477bef9112db4bea9560ef73
Richard Garrison <anm8tr@users.noreply.github.com>
2023-07-16 11:18:23 -0700

    Update JobAttributesResponseDTO.swift

--- Sources/Jobs-App-SharedDTO/JobAttributesResponseDTO.swift
+++ Sources/Jobs-App-SharedDTO/JobAttributesResponseDTO.swift
@@ -21,14 +21,14 @@ public struct JobAttributesResponseDTO: Codable {
     // public let email: String
     // public let contacted: String
     // public let appointmentSet: String
-    // public let appointmentDate: Date
+    public let appointmentDate: Date
     // public let jobStatus: String
     // public let invoiced: String
     // public let invoiceAmount: Double
     // public let notes: String
     
     
-    public init(id: UUID, status: String, name: String, address: String /*city: String, state: String, zipcode: String, phone1: String, phone2: String, email: String, contacted: String, appointmentSet: String, appointmentDate: Date, jobStatus: String, invoiced: String, invoiceAmount: Double, notes: String*/) {
+    public init(id: UUID, status: String, name: String, address: String, /*city: String, state: String, zipcode: String, phone1: String, phone2: String, email: String, contacted: String, appointmentSet: String,*/ appointmentDate: Date /*jobStatus: String, invoiced: String, invoiceAmount: Double, notes: String*/) {
         self.id = id
         self.status = status
         self.name = name
@@ -41,7 +41,7 @@ public struct JobAttributesResponseDTO: Codable {
         // self.email = email
         // self.contacted = contacted
         // self.appointmentSet = appointmentSet
-        // self.appointmentDate = appointmentDate
+        self.appointmentDate = appointmentDate
         // self.jobStatus = jobStatus
         // self.invoiced = invoiced
         // self.invoiceAmount = invoiceAmount
