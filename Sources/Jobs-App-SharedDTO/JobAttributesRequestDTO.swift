ffe9ce4d3043af2a42b8b55f07ee8c79161c5a9e
Richard Garrison <anm8tr@users.noreply.github.com>
2023-07-16 15:40:47 -0700

    Update JobAttributesRequestDTO.swift

--- Sources/Jobs-App-SharedDTO/JobAttributesRequestDTO.swift
+++ Sources/Jobs-App-SharedDTO/JobAttributesRequestDTO.swift
@@ -15,7 +15,7 @@ public struct JobAttributesRequestDTO: Codable {
     public let address: String
     public let city: String
     public let state: String
-    public let zipcode: String
+    public let zipcode: Int
     public let phoneNumberOne: String
     public let phoneNumberTwo: String
     public let email: String
