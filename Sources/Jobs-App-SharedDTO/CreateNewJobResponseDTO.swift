8d705c32447d837b9fcb80b35b07b42c0ccadb28
Richard Garrison <anm8tr@users.noreply.github.com>
2024-04-09 23:03:02 -0700

    Update CreateNewJobResponseDTO.swift

--- Sources/Jobs-App-SharedDTO/CreateNewJobResponseDTO.swift
+++ Sources/Jobs-App-SharedDTO/CreateNewJobResponseDTO.swift
@@ -15,7 +15,7 @@ public struct CreateNewJobResponseDTO: Codable  {
     public let companyName: String
     public let jobId: String
     public let date: Date
-    public let items: [JobAttributesResponseDTO]
+    public var items: [JobAttributesResponseDTO]? = nil
     
     public init(id: UUID, companyName: String, jobId: String, date: Date, items: [JobAttributesResponseDTO] = []) {
         self.id = id
@@ -24,5 +24,26 @@ public struct CreateNewJobResponseDTO: Codable  {
         self.date = date
         self.items = items
     }
+
+    public init(id: UUID, companyName: String, jobId: String, date: Date) {
+        self.id = id
+        self.companyName = companyName
+        self.jobId = jobId
+        self.date = date
+    }
+
+    public init?(createJob: CreateJob) {
+        guard let id = createJob.id else {
+            return nil
+        }
+        
+        if let items = createJob.$items.value {
+            self.init(id: id, companyName: createJob.compaanyName, jobId: createJob.jobId, date: createJob.Date, items: items.compactMap(CreateNewJobResponseDTO.init))
+        } else {
+            self.init(id: id, companyName: createJob.companyName, jobId: createJob.jobId, date: createJob.Date)
+        }
+        
+    }
+    
     
 }
