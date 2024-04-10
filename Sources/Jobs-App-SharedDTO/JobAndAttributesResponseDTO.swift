8e40fc197cd513bea28bc67c24fa1db23a4e6cbf
Richard Garrison <anm8tr@users.noreply.github.com>
2024-04-09 22:21:34 -0700

    Add files via upload

---
+++ Sources/Jobs-App-SharedDTO/JobAndAttributesResponseDTO.swift
@@ -0,0 +1,30 @@
+//
+//  File.swift
+//  
+//
+//  Created by Richard Garrison on 4/9/24.
+//
+
+import Foundation
+
+public struct JobAndAttributesResponseDTO: Codable {
+    
+    public let id: UUID
+    public let companyName: String
+    public let jobId: String
+    public let date: Date
+    public let items: [JobAttributesResponseDTO]
+    
+    public init(id: UUID, companyName: String, jobId: String, date: Date, items: [JobAttributesResponseDTO] = []) {
+        self.id = id
+        self.companyName = companyName
+        self.jobId = jobId
+        self.date = date
+        self.items = items
+    }
+    
+    
+    
+    
+    
+}
